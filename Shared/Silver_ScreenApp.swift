d903df821eaf5534be47e99f43cb2bd494383426
Abbas Mousavi <abbasmousavi@gmail.com>
2020-07-25 16:58:43 +0100

    Initial Commit

---
+++ Shared/Silver_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silver_ScreenApp.swift
+//  Shared
+//
+//  Created by Abbas Mousavi on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Silver_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
